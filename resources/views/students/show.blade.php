3d5d190175beae66b2563a5e7b464d42863168aa
Alejandra Tolaba <alejandra.tolaba02@gmail.com>
2024-05-02 19:41:02 -0300

    add changes show student

--- resources/views/students/show.blade.php
+++ resources/views/students/show.blade.php
@@ -17,13 +17,13 @@
                             <div class="row">
                                 <div class="col-md-12">
                                     <p class="h6"><strong>DNI: </strong>{{ $student->dni }}</p>
-                                    <p class="h6"><strong>Fecha de nacimiento: </strong>{{ $student->birthdate }}</p>
+                                    <p class="h6"><strong>Fecha de nacimiento: </strong>{{ \Carbon\Carbon::parse($student->birthdate)->format('d-m-Y') }}</p>
                                     <p class="h6"><strong>Sexo: </strong>{{ $student->gender == 'F' ? 'Femenino' : 'Masculino' }}</p>
                                     <p class="h6"><strong>Domicilio: </strong>{{ $student->address }}</p>
                                     <p class="h6"><strong>Teléfono: </strong>{{ $student->phone_number }}</p>
                                     <p class="h6"><strong>Contacto: </strong>{{ $student->contact_number ? $student->contact_number : '-' }}</p>
                                     <p class="h6"><strong>Email: </strong>{{ $student->email ? $student->email : '-' }}</p>
-                                    <p class="h6"><strong>Fecha de presentación de certificado: </strong>{{ $student->certificate_date ? $student->certificate_date : 'No presentado' }}</p>
+                                    <p class="h6"><strong>Fecha de presentación de certificado: </strong>{{ $student->certificate_date ? \Carbon\Carbon::parse($student->certificate_date)->format('d-m-Y') : 'No presentado' }}</p>
                                     <p class="h6"><strong>Observaciones: </strong>{{ $student->observations ? $student->observations : 'Sin observaciones' }}</p>
                                     <p class="h6"><strong>Rutina: </strong>{{ $student->routine ? $student->routine : 'Sin rutina' }}</p>
                                 </div>

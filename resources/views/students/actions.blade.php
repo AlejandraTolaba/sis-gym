fd3f2a685f7f4b1236798fdb5d99bcbb3afc29eb
Alejandra Tolaba <alejandra.tolaba02@gmail.com>
2024-05-21 19:52:28 -0300

    add changes

--- resources/views/students/actions.blade.php
+++ resources/views/students/actions.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('students.show',$id) }}"><button name="show-dates" title="Ver datos de alumno" type="submit" class="btn btn-success"><i class="fa fa-user"></i> Visualizar</button></a>
-<a href="{{ route('students.edit',$id) }}"><button name="edit" title="Editar alumno" type="submit" class="btn btn-warning"><i class="fas fa-pencil-alt"></i> Editar</button></a>
-<a href="{{route('inscriptions',$id)}}"><button name="index" type="submit" title="Ver inscripciones" class="btn btn-info"><i class="fa fa-eye"></i> Inscripciones</button></a>
-<a href="{{ route('bodychecks',$id) }}"><button name="show" title="Ver fichas de control corporal" type="submit" class="btn btn-primary"><i class="fa fa-file"></i> Fichas de control</button></a>
+<a href="{{ route('students.show',$id) }}"><button name="show-dates" title="Ver datos de alumno" type="submit" class="btn btn-success btn-sm"><i class="fa fa-user"></i> Visualizar</button></a>
+<a href="{{ route('students.edit',$id) }}"><button name="edit" title="Editar alumno" type="submit" class="btn btn-warning btn-sm"><i class="fas fa-pencil-alt"></i> Editar</button></a>
+<a href="{{route('inscriptions',$id)}}"><button name="index" type="submit" title="Ver inscripciones" class="btn btn-info btn-sm"><i class="fa fa-eye"></i> Inscripciones</button></a>
+<a href="{{ route('bodychecks',$id) }}"><button name="show" title="Ver fichas de control corporal" type="submit" class="btn btn-primary btn-sm"><i class="fa fa-file"></i> Fichas de control</button></a>

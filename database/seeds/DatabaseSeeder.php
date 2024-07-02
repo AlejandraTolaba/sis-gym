9788dcdca85eea6abe8cad991a9aaf8b4d856ccb
Alejandra Tolaba <alejandra.tolaba02@gmail.com>
2024-07-02 18:00:15 -0300

    add seeder user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(UserSeeder::class);
         $this->call(ActivitySeeder::class);
         $this->call(StudentSeeder::class);
         $this->call(PlanSeeder::class);
